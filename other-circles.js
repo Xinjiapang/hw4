76d38b724b089306d1a94e6ae53f94d353e2ace6
Xinjiapang <47263250+Xinjiapang@users.noreply.github.com>
2019-02-24 00:13:54 -0800

    Create other-circles.js

---
+++ other-circles.js
@@ -0,0 +1,30 @@
+function setup() {
+  createCanvas(400, 400);
+  colorMode(HSB);
+}
+
+var x = []; // new empty array
+var y = []; // new empty array
+
+function draw() {
+  background(255);
+  noFill();
+
+  x.push(mouseX); // equivalent to append(x, mouseX)
+  y.push(mouseY); // equivalent to append(y, mouseY)
+
+  for (var i = 0; i < x.length; i = i + 10) {
+    fill(random(x),200,100);
+    beginShape();
+    vertex(x[i], y[i]);
+    vertex(x[i]+(x.length-i), y[i]);
+    vertex(x[i]+(x.length-i), y[i]+(x.length-i));
+    vertex(x[i]+(x.length-i)*2, y[i]+(x.length-i));
+    vertex(x[i]+(x.length-i)*2, y[i]+(x.length-i)*2);
+    vertex(x[i], y[i]+(x.length-i)*2);
+    endShape(CLOSE);
+  }
+
+  x = x.slice(-50); // keep the last 50 x values
+  y = y.slice(-50); // keep the last 50 y values
+}
